2c92571977c6557b4d55d24f74f5f0776a58a26a
Chris J Abraham <ajsirch@yahoo.com>
2024-08-29 11:17:14 -0400

    Updates to search form

--- src/components/Hero.js
+++ src/components/Hero.js
@@ -2,7 +2,7 @@ import React from 'react';
 import '../App.css';
 import { TrySearchButton } from './TrySearchButton';
 import './Hero.css';
-import { Drawer, Button, Placeholder } from 'rsuite';
+import { Drawer, Button, Placeholder, Col } from 'rsuite';
 import { Form, ButtonToolbar, Input } from 'rsuite';
 import { FlexboxGrid, Divider } from 'rsuite';
 
@@ -17,21 +17,24 @@ function Hero() {
   };
 
   return (
-    <div className='hero-container'>
-      <video src='/videos/video-6.mp4' autoPlay loop muted />
-      <h2>ARDENT finds Answers</h2>
-      <p>Get the latest research and clinical trials information on any Rare Disease</p>
-      <div className='hero-btns'>
-        {!open && <TrySearchButton           
-          className='btns'
-          buttonStyle='btn--primary'
-          buttonSize='btn--large'
-          onClick={() => handleOpen('bottom')}
-        >
-          Try a Search!
-        </TrySearchButton>}
+    <div>
+      <div className='hero-container'>
+        <video src='/videos/video-6.mp4' autoPlay loop muted />
+        <h2>ARDENT finds Answers</h2>
+        <p>Get the latest research and clinical trials information on any Rare Disease</p>
+        <div className='hero-btns'>
+          {!open && 
+            <TrySearchButton           
+              className='btns'
+              buttonStyle='btn--primary'
+              buttonSize='btn--large'
+              onClick={() => handleOpen('bottom')}
+            >
+              Try a Search!
+            </TrySearchButton>}
+        </div>
       </div>
-      <div style={{backgroundColor:'#fff'}}>
+      <div className='hero-container-search'>
           <Drawer size='md' placement={placement} open={open} onClose={() => setOpen(false)}>
             <Drawer.Header>
               <Drawer.Title>Search latest information about Rare Dieases</Drawer.Title>
@@ -39,28 +42,29 @@ function Hero() {
                 <Button onClick={() => setOpen(false)}>Cancel</Button>  
               </Drawer.Actions>
             </Drawer.Header>
-            <Drawer.Body>            
-              <Form>
-                <FlexboxGrid justify="space-between">
-                  <FlexboxGrid.Item colspan={6}/>
-                  <FlexboxGrid.Item colspan={6}>
-                    <Form.Group controlId="textarea">
-                      <Form.ControlLabel>Enter your question:</Form.ControlLabel>
-                      <Form.Control rows={5} name="textarea" accepter={Textarea} />
-                    </Form.Group>
-                  </FlexboxGrid.Item>
-                  <FlexboxGrid.Item colspan={6}/>                  
-                </FlexboxGrid>
-                <FlexboxGrid justify="space-between">
-                  <ButtonToolbar>
-                    <FlexboxGrid.Item colspan={4}><Button appearance="primary">Submit</Button></FlexboxGrid.Item>
-                    <FlexboxGrid.Item colspan={4}><Button appearance="default">Cancel</Button></FlexboxGrid.Item>
-                  </ButtonToolbar>                
-                </FlexboxGrid>                
-              </Form>
+            <Drawer.Body>
+              <div>
+                <Form layout="horizontal">
+                  <FlexboxGrid justify="center">
+                    <FlexboxGrid.Item as={Col} colspan={24} md={6}></FlexboxGrid.Item>
+                    <FlexboxGrid.Item as={Col} colspan={24} md={6}>
+                      <Form.Group controlId="textarea">
+                        <Form.ControlLabel>Enter your question:</Form.ControlLabel>
+                        <Form.Control rows={5} name="textarea" accepter={Textarea} />
+                      </Form.Group>
+                    </FlexboxGrid.Item>
+                    <FlexboxGrid.Item as={Col} colspan={24} md={6}></FlexboxGrid.Item>
+                  </FlexboxGrid>
+                  <FlexboxGrid justify="center">
+                    <ButtonToolbar>
+                      <FlexboxGrid.Item as={Col} colspan={24} md={6}><Button appearance="primary">Search</Button></FlexboxGrid.Item>
+                    </ButtonToolbar>                
+                  </FlexboxGrid>                
+                </Form>
+                </div>
             </Drawer.Body>
           </Drawer>
-        </div>      
+      </div>
     </div>
   );
 }
