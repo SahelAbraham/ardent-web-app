8af59f37d4f5bcc54dded74b799629490541c6bc
Chris J Abraham <ajsirch@yahoo.com>
2024-11-28 23:43:53 -0500

    Add contact initial impl

--- src/components/workspace/contact/Contact.js
+++ src/components/workspace/contact/Contact.js
@@ -11,7 +11,18 @@ export default function Contact(){
         <SideBarNav/>
       </div>
       <div className="flex-child-right">
-        <div>Contact</div>  
+        <div className='generic-search-container-description'>
+          <h4>Contact</h4>
+          <div style={{"marginBottom" : "5px"}}>
+              <h6>
+                <p>There are several ways to reach out to us:</p>
+                <ul>
+                  <li><p>Use the Chat at the lower right hand side of the screen.</p></li>
+                  <li><p>You can also email directly at: ardent@novomorpho.com</p></li>
+                </ul>                
+              </h6>
+          </div>
+        </div>
       </div>
     </div>    
     );
